19ab2324d436ac9db44e732aee3e33fd8b465e24
Pikkel <76259034+Pikkel@users.noreply.github.com>
2021-11-08 08:56:48 -0500

    added delay x2

--- DolphinWithConfig/src/main.rs
+++ DolphinWithConfig/src/main.rs
@@ -4,6 +4,7 @@ use std::collections::HashMap;
 use std::fs::File;
 use std::io;
 use std::io::Read;
+use std::{thread, time::Duration};
 use webbrowser;
 
 #[tokio::main]
@@ -15,6 +16,7 @@ async fn main() {
     let mut webhook = String::new();
     let mut input = String::new();
     let mut times = String::new();
+    let mut delay = String::new();
     let hooksend = 0;
     let hookrem = 1;
 
@@ -34,6 +36,11 @@ async fn main() {
             .read_line(&mut times)
             .expect("I'm gonna say the nword");
         let timeint: i32 =  times.trim().parse::<i32>().expect("I'm gonna say the nword");
+        println!("\nDelay:");
+        io::stdin()
+            .read_line(&mut delay)
+            .expect("I'm gonna say the nword");
+        let delayint: u64 =  delay.trim().parse::<u64>().expect("I'm gonna say the nword");
 
         let mut file = File::open("config.json").unwrap();
         let json: serde_json::Value = serde_json::from_reader(file).expect("I'm gonna say the nword");
@@ -44,6 +51,7 @@ async fn main() {
         for i in 0..timeint {
             println!("Sending: {}", i);
             client.post(&webhook).json(&json).send().await;
+            thread::sleep(Duration::from_millis(delayint));
         }
         println!("\nFinished")
     } else if n == 1 {
